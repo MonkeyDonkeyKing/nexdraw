af81b37745fad28d6218f9507aa0d805459ff91c
MickSol <mikkertreserve@gmail.com>
2023-10-24 13:02:42 +0000

    emperor test updates

--- tests/01-program_initialization/emperor.spec.ts
+++ tests/01-program_initialization/emperor.spec.ts
@@ -6,22 +6,23 @@ import { NexDraw } from '../../typescript/src';
 import { Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
 
 describe('Emperor functionalities', () => {
-  describe('initializes the emperor', () => {
-    // Removed async here
-    it('Sends initialization function for the emperor', async () => {
+  let emperorAddress = EMPEROR_ADDRESS[0];
+
+  describe('Initialization', () => {
+    it('should send an initialization function for the emperor', async () => {
       const a = await client.createEmperor();
       assert.ok(a);
     });
-    it('Checks if the emperor is initialized', async () => {
-      const emperor = await client.program.account.emperor.fetch(EMPEROR_ADDRESS[0]);
+
+    it('should check if the emperor is initialized', async () => {
+      const emperor = await client.program.account.emperor.fetch(emperorAddress);
       assert.ok(emperor.authority.equals(client.provider.publicKey));
     });
-  });
-  describe('updates the emperor', () => {
-    it('calls initialize more than once', async () => {
+
+    it('should not allow initializing the emperor more than once', async () => {
       try {
         await client.createEmperor();
-        assert.ok(false);
+        assert.fail('Expected an error, but none was thrown.');
       } catch (err) {
         const e = err as anchor.web3.SimulatedTransactionResponse;
         assert.strictEqual(
@@ -31,22 +32,30 @@ describe('Emperor functionalities', () => {
       }
     });
   });
-  describe('tries to hack the emperor', () => {
+
+  describe('Updates', () => {
+    // Include tests specific to emperor updates here
+  });
+
+  describe('Security', () => {
     const hacker_one = new NexDraw(
       new anchor.AnchorProvider(client.provider.connection, new anchor.Wallet(Keypair.generate()), {})
     );
+
     const hacker_two = new NexDraw(
       new anchor.AnchorProvider(client.provider.connection, new anchor.Wallet(Keypair.generate()), {})
     );
+
     before(async () => {
       await client.provider.connection.requestAirdrop(hacker_one.provider.publicKey, 1 * LAMPORTS_PER_SOL);
       await client.provider.connection.requestAirdrop(hacker_two.provider.publicKey, 1 * LAMPORTS_PER_SOL);
       await wait(1000);
     });
-    it('hacker tries to reinitialize the emperor', async () => {
+
+    it('should prevent unauthorized reinitialization of the emperor', async () => {
       try {
         await hacker_one.createEmperor();
-        assert.ok(false);
+        assert.fail('Expected an error, but none was thrown.');
       } catch (err) {
         const e = err as anchor.web3.SimulatedTransactionResponse;
         assert.strictEqual(
@@ -55,14 +64,17 @@ describe('Emperor functionalities', () => {
         );
       }
     });
-    it("hacker tries to update the emperor's authority", async () => {
+
+    it("should prevent unauthorized users from updating the emperor's authority", async () => {
       try {
         await hacker_two.updateEmperor(hacker_two.provider.publicKey);
-        assert.ok(false);
+        assert.fail('Expected an error, but none was thrown.');
       } catch (err) {
         const e = err as anchor.AnchorError;
         assert.strictEqual(e.error.errorMessage, 'A has one constraint was violated');
       }
     });
   });
+
+  // If there are any other categories of tests, you can add another describe block similar to the ones above
 });
